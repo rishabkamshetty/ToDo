5f6ca5a269bab41530daf08a6504e776b378095c
Rishab <rishabkamshetty@gmail.comy>
2019-01-15 03:14:21 -0800

    Done on Check, Input value restrict

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Skrike the ToDo list on CLick
-$("ul").on("click", ".checkboxSpan", function () {
-    $(this).parent().toggleClass("strike");
+$("ul").on("click", ".doneCheck", function () {
+    $(this).siblings().find("label").toggleClass("strike");
 });
 // On click of trash icon, delete clicked ToDo list
 $("ul").on("click", ".iconSpan", function (e) {
@@ -10,7 +10,7 @@ $("ul").on("click", ".iconSpan", function (e) {
     e.stopPropagation();
 });
 
-//  Adding list from input
+/* //  Adding list from input
 $("#addNewList").on("keypress", function (e) {
     // On pressing Enter
     if (e.which === 13) {
@@ -18,9 +18,30 @@ $("#addNewList").on("keypress", function (e) {
         var newToDo = $(this).val();
         $(this).val("");
         // Add a new list and add to ul
-        $("ul").append("<li class='lists'><span class='iconSpan'><i class='far fa-trash-alt fa-xs listDelete' aria-hidden='true'></i></span><span class='checkboxSpan'><input type='checkbox' name='checkboxDone' class='doneCheck'></span>"+" "+newToDo+"</li>");
+        $("ul").append("<li class='lists'><span class='iconSpan'><i class='far fa-trash-alt fa-xs listDelete' aria-hidden='true'></i></span>   <span class='pretty p-icon p-curve p-pulse'>  <input type='checkbox' class='doneCheck'>  <span class='state p-success'><i class='icon mdi mdi-check'></i><label>" + " " + newToDo + "</label></span></span></li>");
     }
 });
+ */
+
+//  Adding list from input with wrap
+$("#addNewList").on("keypress", function (e) {
+    // chech the input length
+    if ($(this).val().length < 26) {
+        // On pressing Enter
+        if (e.which === 13) {
+            // retrive the input value
+            var newToDo = $(this).val();
+            $(this).val("");
+            // Add a new list and add to ul
+            $("ul").append("<li class='lists'><span class='iconSpan'><i class='far fa-trash-alt fa-xs listDelete' aria-hidden='true'></i></span>   <span class='pretty p-icon p-curve p-pulse'>  <input type='checkbox' class='doneCheck'>  <span class='state p-success'><i class='icon mdi mdi-check'></i><label>" + " " + newToDo + "</label></span></span></li>");
+        }
+    } else {
+        alert(" Numbers of charaters are limited 25");
+    }
+
+});
+
+
 
 // On click of Plus, toggle input box
 
